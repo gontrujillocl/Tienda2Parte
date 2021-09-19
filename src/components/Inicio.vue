68807731726b44171e5e23768c555c835ed60e38
gontrujillocl <gonzalo@truall.com>
2021-09-19 16:14:17 -0400

    Segunda parte arriba

---
+++ src/components/Inicio.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="Inicio">
+    <div class="container text-center mt-4">
+      <h1>32bits</h1>
+      <h3><b>Juegos de PC</b> y Consolas</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Inicio'
+}
+</script>
+
+<style></style>
